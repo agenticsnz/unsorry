859ed4e64dc8dddc267d27e7972e61079bd7d5df
Chris Barlow <959402+cgbarlow@users.noreply.github.com>
2026-06-26 04:59:40 +1200

    decompose(putnam-1967-a5): 4 sub-lemmas by afnz-zbook-b336

---
+++ goals/putnam-1967-a5-s2.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem putnam_1967_a5_isodiametric_area_le (R : Set (EuclideanSpace ℝ (Fin 2))) (hd : ∀ P ∈ R, ∀ Q ∈ R, dist P Q ≤ 1) : (MeasureTheory.volume R).toReal ≤ Real.pi / 4 := by
+  sorry
