673c69aa0c0ae08e41b8fa54d30b67ac53b2c696
Chris Barlow <959402+cgbarlow@users.noreply.github.com>
2026-06-26 11:09:03 +1200

    decompose(putnam-1966-a1): 3 sub-lemmas by afnz-zbook-b336

---
+++ goals/putnam-1966-a1-s1.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem putnam_1966_a1_step (n : ℤ) (hn : 0 ≤ n) : n ^ 2 / 4 + (if Even (n + 1) then (n + 1) / 2 else (n + 1 - 1) / 2) = (n + 1) ^ 2 / 4 := by
+  sorry
---
+++ goals/putnam-1966-a1-s3.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem putnam_1966_a1_quad_div (x y : ℤ) : (x + y) ^ 2 / 4 - (x - y) ^ 2 / 4 = x * y := by
+  sorry
